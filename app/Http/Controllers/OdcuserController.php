48cc4e6409af51518746c058ed55bc671738c427
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-11-29 16:56:09 +0100

    Optimisation de la fonction de recherche pour prendre en compte la recherche par le prenom et le nom en meme temps.

--- app/Http/Controllers/OdcuserController.php
+++ app/Http/Controllers/OdcuserController.php
@@ -25,10 +25,18 @@ class OdcuserController extends Controller
     public function search(Request $request)
     {
         $searchTerm = $request->input('search');
-        $odcusers = Odcuser::where('first_name', 'LIKE', "%{$searchTerm}%")
-            ->orWhere('last_name', 'LIKE', "%{$searchTerm}%")
-            ->orWhere('email', 'LIKE', "%{$searchTerm}%")
-            ->take(4)
+        $searchTerms = explode(' ', $searchTerm);
+
+        $query = Odcuser::query();
+
+        foreach ($searchTerms as $term) {
+            $query->where(function ($q) use ($term) {
+                $q->where('first_name', 'LIKE', "%{$term}%")
+                ->orWhere('last_name', 'LIKE', "%{$term}%");
+            });
+        }
+
+        $odcusers = $query->take(4)
             ->latest()
             ->get();
 
